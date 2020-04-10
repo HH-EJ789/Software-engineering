2c4b640bb13e8f327fe79ad6ba9c7685c9a964a0
黄恩俊 <1021217094@qq.com>
2020-04-10 10:14:19 +0800

    测试

---
+++ software-engineering-web/src/test/java/com/hhej789/softwareengineering/SoftwareEngineeringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hhej789.softwareengineering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareEngineeringWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
