2f49ae04f650807802d69d0fc044867b329ac163
黄恩俊 <1021217094@qq.com>
2020-04-22 20:51:41 +0800

    aichi

---
+++ software-engineering-web/src/main/java/com/hhej789/softwareengineering/bean/Admin.java
@@ -0,0 +1,12 @@
+package com.hhej789.softwareengineering.bean;
+
+import lombok.Data;
+
+@Data
+public class Admin {
+
+    private String admin_id;
+    private String password;
+    private String admin_name;
+
+}
