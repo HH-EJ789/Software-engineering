0b0b112983e884d4209a0f8d57b48e9739d1a123
Groggen <1062302920@qq.com>
2020-04-22 21:03:29 +0800

    gyb commit

---
+++ software-engineering-web/src/main/java/com/hhej789/softwareengineering/bean/Info.java
@@ -0,0 +1,20 @@
+package com.hhej789.softwareengineering.bean;
+
+/**
+ * @author gybin___NN
+ * @data 2020 - 04 - ${DATA} - 20:53
+ */
+public class Info {
+
+    private Integer info_id;
+    private Integer sort_id;
+    private Integer public_member_id;
+    private String good_name;
+    private String good_description;
+    private Integer status;
+    private Integer scan_num;
+    private Integer deal_member_id;
+
+
+
+}
---
+++ software-engineering-web/src/main/java/com/hhej789/softwareengineering/bean/InfoSort.java
@@ -0,0 +1,12 @@
+package com.hhej789.softwareengineering.bean;
+
+/**
+ * @author gybin___NN
+ * @data 2020 - 04 - ${DATA} - 20:54
+ */
+public class InfoSort {
+
+    private Integer sort_id;
+    private String  sort_name;
+
+}
