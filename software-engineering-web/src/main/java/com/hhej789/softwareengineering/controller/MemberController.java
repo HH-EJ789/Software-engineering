9a954e957a1b1b6841ad1b8de5424a08e45776ab
黄恩俊 <1021217094@qq.com>
2020-04-23 10:46:00 +0800

    aichi

---
+++ software-engineering-web/src/main/java/com/hhej789/softwareengineering/controller/MemberController.java
@@ -0,0 +1,11 @@
+package com.hhej789.softwareengineering.controller;
+
+/**
+ * @ClassName MemberController
+ * @Description TODO
+ * @Author AICHI
+ * @Date 2020/4/23 10:36
+ * @Version 1.0
+ */
+public class MemberController {
+}
---
+++ software-engineering-web/src/main/java/com/hhej789/softwareengineering/mapper/MemberMapper.java
@@ -0,0 +1,11 @@
+package com.hhej789.softwareengineering.mapper;
+
+/**
+ * @ClassName MemberMapper
+ * @Description TODO
+ * @Author AICHI
+ * @Date 2020/4/23 10:37
+ * @Version 1.0
+ */
+public class MemberMapper {
+}
