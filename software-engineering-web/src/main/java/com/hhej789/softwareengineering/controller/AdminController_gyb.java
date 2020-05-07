4ce0bdd50241308f0710ab173600c7b6286128ed
黄恩俊 <1021217094@qq.com>
2020-05-07 11:15:55 +0800

    aichi

--- software-engineering-web/src/main/java/com/hhej789/softwareengineering/controller/AdminController_gyb.java
+++ software-engineering-web/src/main/java/com/hhej789/softwareengineering/controller/AdminController_gyb.java
@@ -1,22 +1,13 @@
-<<<<<<< Updated upstream
 package com.hhej789.softwareengineering.controller;
 
-import com.hhej789.softwareengineering.bean.Admin;
 import com.hhej789.softwareengineering.mapper.AdminMapper_gyb;
 import com.hhej789.softwareengineering.mapper.InfoSortMapper_gyb;
 import com.hhej789.softwareengineering.mapper.MemberMapper_gyb;
-import com.hhej789.softwareengineering.tools.MD5Util_hej;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
-import java.util.UUID;
-import java.util.concurrent.TimeUnit;
 
 /**
  * @author gybin___NN
@@ -96,137 +87,3 @@ public class AdminController_gyb {
     }
 
 }
-
-=======
-//package com.hhej789.softwareengineering.controller;
-//
-//import com.hhej789.softwareengineering.mapper.AdminMapper_gyb;
-//import com.hhej789.softwareengineering.service.AdminService_gyb;
-//import com.hhej789.softwareengineering.tools.MD5Util_hej;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.*;
-//
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpSession;
-//import java.util.HashMap;
-//import java.util.Map;
-//import java.util.Objects;
-//import java.util.UUID;
-//import java.util.concurrent.TimeUnit;
-//
-///**
-// * @author gybin___NN
-// * @data 2020 - 04 - ${DATA} - 00:25
-// */
-//@Controller
-//public class AdminController_gyb {
-//
-//    @Autowired
-//    public AdminService_gyb adminService_gyb;
-//
-//    @Autowired
-//    public AdminMapper_gyb adminMapper_gyb;
-//
-//
-//    @RequestMapping(value = "/", method = {RequestMethod.POST, RequestMethod.GET})
-//    public String Login_Null() {
-//        return "/login/login";
-//    }
-//
-//    @RequestMapping(value = "/AdminloginPage", method = {RequestMethod.POST, RequestMethod.GET})
-//    public String Login_admin(HttpServletRequest request, HttpSession session) {
-//        String admin_id = request.getParameter("admin_id");
-//        String password = request.getParameter("password");
-//        System.out.println("你输入的用户名为：" + admin_id);
-//        System.out.println("你输入的密码为：" + password);
-//        String admin_name = adminService_gyb.Login_admin(admin_id, password);
-//        session.setAttribute("admin_name", admin_name);        //绑定登陆后的人，用于查看管理员
-//        if (admin_id == null) {
-//            System.out.println("账号或密码有误，请重新登陆");
-//            return "redirect:/";
-//
-//        } else {
-//            return "redirect:/AdminService";             //进入管理员操作页面
-//        }
-//    }
-//
-//    @GetMapping("/AdminService/FindMember")
-//    public Map<String, Object> Find_member(@RequestParam("member_id") String member_id) {
-//        Map map = new HashMap();
-//        try {
-//            map.put("data", adminMapper_gyb.Find_member(member_id));
-//            System.out.println("查询成功！");
-//            map.put("msg", "查询成功");
-//        } catch (Exception e) {
-//            System.out.println(e);
-//            map.put("msg", "查询失败");
-//        }
-//        return map;
-//
-//    }
-//
-//    @GetMapping("/AdminService/FindInfo")
-//
-//    public Map<String, Object> Find_Info(@RequestParam("info_id") String info_id) {
-//        Map map = new HashMap();
-//        try {
-//            map.put("data", adminMapper_gyb.Find_info(info_id));
-//            System.out.println("查询成功！");
-//            map.put("msg", "查询成功");
-//        } catch (Exception e) {
-//            System.out.println(e);
-//            map.put("msg", "查询失败");
-//        }
-//        return map;
-//
-//    }
-//
-//    @GetMapping("/AdminService/FindInfo_sort")
-//
-//    public Map<String, Object> Find_infoBysort(@RequestParam("sort_name") String sort_name) {
-//        Map map = new HashMap();
-//        try {
-//
-//            map.put("data", adminMapper_gyb.Find_infoBysort(sort_name));
-//            System.out.println("查询成功！");
-//            map.put("msg", "查询成功");
-//        } catch (Exception e) {
-//            System.out.println(e);
-//            map.put("msg", "查询失败");
-//        }
-//        return map;
-//    }
-//
-//    @GetMapping("/AdminService/Findrequest")
-//
-//    public Map<String, Object> Find_Request(@RequestParam("request_id") String request_id) {
-//        Map map = new HashMap();
-//        try {
-//            map.put("data", adminMapper_gyb.Find_infoBysort(request_id));
-//            System.out.println("查询成功！");
-//            map.put("msg", "查询成功");
-//        } catch (Exception e) {
-//            System.out.println(e);
-//            map.put("msg", "查询失败");
-//        }
-//        return map;
-//
-//    }
-//
-//    @GetMapping("/AdminService/Find_requestBysort")
-//    public Map<String, Object> Find_requestBysort(@RequestParam("sort_name") String sort_name) {
-//        Map map = new HashMap();
-//        try {
-//            map.put("data", adminMapper_gyb.Find_requestBysort(sort_name));
-//            System.out.println("查询成功！");
-//            map.put("msg", "查询成功");
-//        } catch (Exception e) {
-//            System.out.println(e);
-//            map.put("msg", "查询失败");
-//        }
-//        return map;
-//    }
-//}
-//
->>>>>>> Stashed changes
--- software-engineering-web/src/main/java/com/hhej789/softwareengineering/mapper/AdminMapper_gyb.java
+++ software-engineering-web/src/main/java/com/hhej789/softwareengineering/mapper/AdminMapper_gyb.java
@@ -1,12 +1,7 @@
-<<<<<<< Updated upstream
 package com.hhej789.softwareengineering.mapper;
 
 import com.hhej789.softwareengineering.bean.*;
-import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -37,44 +32,3 @@ public interface AdminMapper_gyb {
     public Integer getInfoMemIdbyNum(Integer member_id);
 
 }
-=======
-//package com.hhej789.softwareengineering.mapper;
-//
-//import com.hhej789.softwareengineering.bean.*;
-//import org.apache.ibatis.annotations.Insert;
-//import org.apache.ibatis.annotations.Mapper;
-//import org.apache.ibatis.annotations.Param;
-//import org.apache.ibatis.annotations.Select;
-//import org.springframework.stereotype.Component;
-//import org.springframework.stereotype.Repository;
-//
-///**
-// * @author gybin___NN
-// * @data 2020 - 04 - ${DATA} - 16:07
-// */
-//@Mapper
-//@Component
-//public interface AdminMapper_gyb {
-//
-//    @Select("select * from Admin where admin_id=#{admin_id}")
-//    public String Login_admin(@Param("admin_id") String admin_id, @Param("password") String password);
-//
-//    @Select("select * from Member where member_id=#{member_id}")
-//    public String Find_member(String member_id);
-//
-//    @Select("select * from Info where info_id=#{info_id}")
-//    public String Find_info(String info_id);
-//
-//    @Select("select * from Info RIGHT JOIN  InfoSort ON Info.sort_id = InfoSort.sort_id AND sort_name=#{sort_name}")
-//    public String Find_infoBysort( String sort_id);
-//
-//    @Select("select * from RequestExchange where request_id=#{request_id}")
-//    public String Find_request(String request_id);
-//
-//    @Select("select * from RequestExchange LEFT JOIN Info ON RequestExchange.info_id = Info.info_id LEFT JOIN InfoSort ON Info.sort_id = InfoSort.sort_id AND InfoSort.sort_name = #{sort_name}")
-//    public String Find_requestBysort(String sort_name);
-//
-//
-//
-//}
->>>>>>> Stashed changes
